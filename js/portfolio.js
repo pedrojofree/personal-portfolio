24adad5367e2ad9555aeb92e4721ec0688019802
Pedro Jofre <pedrojofre707@gmail.com>
2023-01-14 18:28:48 -0300

    first commit

---
+++ js/portfolio.js
@@ -0,0 +1,28 @@
+const projectos = [
+    {
+        titulo: "",
+        tecnologias: "",
+        descripcion: "",
+        link: ""
+    },
+    {
+        titulo: "",
+        tecnologias: "",
+        descripcion: "",
+        link: ""
+    },
+    {
+        titulo: "",
+        tecnologias: "",
+        descripcion: "",
+        link: ""
+    },
+    {
+        titulo: "",
+        tecnologias: "",
+        descripcion: "",
+        link: ""
+    }
+]
+
+console.log(projectos);
